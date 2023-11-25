56b9cf37fc68f46064c1b253e025c8b770c28ea7
Nihadname <nihadcoding@gmail.com>
2023-11-25 13:24:52 +0400

    Add project files.

---
+++ ConsoleApp18/Models/Doctor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18.Models
+{
+    internal class Doctor
+    {
+        public string Name { get; set; }
+        public  string SurName { get; set; }
+        public string  Address { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Doctor(string name,string surname,string address,DateTime birthday) {
+        Name = name;
+            SurName = surname;
+            Address = address;
+            Birthday = birthday;
+        
+        }
+        public Doctor() { }
+        public Doctor[] info()
+        {
+            Doctor doctor1 = new Doctor("Nihad", "Ibadzade", "Baki", new DateTime(2008, 2, 21));
+            Doctor doctor2 = new Doctor("Kazim", "Ibadzadov", "samaxi", new DateTime(2001, 2, 21));
+            Doctor doctor3 = new Doctor("kamil", "filankesov", "gence", new DateTime(1902, 2, 21));
+
+            Doctor[] doctors = { doctor1, doctor2, doctor3 };
+            return doctors;
+        }
+        public void GettingDoctors()
+        {
+            Doctor[] doctorsInfo=info();
+            DateTime date1 = new DateTime(1993, 5, 25);
+            DateTime date2 = new DateTime(2002, 12, 15);
+            foreach (Doctor doctor in doctorsInfo)
+            {
+                if (doctor.Birthday > date1 && doctor.Birthday < date2)
+                {
+                    Console.WriteLine($"{doctor.Name} {doctor.SurName} {doctor.Address} {doctor.Birthday}");
+                }
+
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp18.Models;
+
+Console.WriteLine("Hello, World!");
+//Doctor doctor1 = new Doctor("Nihad", "Ibadzade", "Baki", new DateTime(2008, 2, 21));
+//Doctor doctor2 = new Doctor("Kazim", "Ibadzadov", "samaxi", new DateTime(2001, 2, 21));
+//Doctor doctor3 = new Doctor("kamil", "filankesov", "gence", new DateTime(1902, 2, 21));
+
+//Doctor[] doctors = {doctor1, doctor2, doctor3};
+//DateTime date1 = new DateTime(1993, 5, 25);
+//DateTime date2 = new DateTime(2002, 12, 15);
+//void GettingDoctors(DateTime m,DateTime n) {
+//    foreach (Doctor doctor in doctors)
+//    {
+//        if(doctor.Birthday>m&&doctor.Birthday<n)
+//        {
+//            Console.WriteLine($"{doctor.Name} {doctor.SurName} {doctor.Address} {doctor.Birthday}");
+//        }
+
+//    }
+//}
+
+//GettingDoctors(date1, date2);
+Doctor doctor = new();
+doctor.info();
+doctor.GettingDoctors();
